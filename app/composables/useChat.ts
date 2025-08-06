e299d0a88b0e436ed04c77239c48ce862b5ef590
CristianNorga <cristian.rraga95@hotmail.com>
2025-08-06 07:48:27 -0500

    feat: add composable useChat, componentes ChatInput and ChatWindows

--- app/composables/useChat.ts
+++ app/composables/useChat.ts
@@ -2,30 +2,31 @@ import type { Chat, ChatMessage } from '../types';
 import { MOCK_CHAT } from './mockData';
 
 export default function useChat() {
-	const chat = ref<Chat>({ ...MOCK_CHAT });
-	const messages = computed(() => chat.value.messages);
+	const chat = ref<Chat>(MOCK_CHAT);
+	const messages = computed<ChatMessage[]>(() => chat.value.messages);
 
-	const createMessage = (
-		content: string,
-		role: 'user' | 'assistant'
-	): ChatMessage => {
+	chat.value.messages = [];
+
+	function createMessage(message: string, role: ChatMessage['role']) {
+		const id = messages.value.length.toString();
+
 		return {
-			id: (chat.value.messages.length + 1).toString(),
+			id,
 			role,
-			content,
+			content: message,
 		};
-	};
+	}
 
-	const sendMessage = (message: string) => {
+	async function sendMessage(message: string) {
 		messages.value.push(createMessage(message, 'user'));
-		// Simulate assistant response
 
+		// Simulate an API call to send the message
 		setTimeout(() => {
-			messages.value.push(
-				createMessage('This is a mock response from the assistant.', 'assistant')
-			);
-		}, 200);
-	};
+			const response = `Response to: ${message}`;
+
+			messages.value.push(createMessage(response, 'assistant'));
+		}, 1000);
+	}
 
 	return {
 		chat,
--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,14 +1,3 @@
-<script lang="ts" setup>
-definePageMeta({
-	layout: false,
-});
-</script>
-
 <template>
-	<div>
-		<h1>minkura chat :D</h1>
-		<NuxtLayout name="primary">
-			<p>This is the chat page content.</p>
-		</NuxtLayout>
-	</div>
+	<ChatWindow />
 </template>
