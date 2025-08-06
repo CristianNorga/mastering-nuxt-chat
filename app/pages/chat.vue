23649349e42790efc13e5c55217a023c90fafc22
CristianNorga <cristian.rraga95@hotmail.com>
2025-08-06 06:45:34 -0500

    feat: layouts

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+definePageMeta({
+	layout: false,
+});
+</script>
+
 <template>
-	<h1>minkura chat :D</h1>
+	<div>
+		<h1>minkura chat :D</h1>
+		<NuxtLayout name="primary">
+			<p>This is the chat page content.</p>
+		</NuxtLayout>
+	</div>
 </template>
