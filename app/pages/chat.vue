2b46d7e65633d39b623344ef0e0e052bba5a4625
CristianNorga <cristian.rraga95@hotmail.com>
2025-08-11 06:42:03 -0500

    feat: add meta info and title page

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 const { chat, messages, sendMessage } = useChat();
+
+useHead({
+	title: chat.value.title,
+	meta: [
+		{ name: 'name', content: 'Chat with our AI' },
+		{ name: 'description', content: 'Project Mastering Nuxt' },
+	],
+});
 </script>
 
 <template>
