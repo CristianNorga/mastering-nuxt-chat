d4e42fe03d431431ed038ac85dd72dca14c7ecb5
CristianNorga <cristian.rraga95@hotmail.com>
2025-08-12 06:29:33 -0500

    chore: Configure Secure and Clear Runtime Settings in Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,11 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
 	modules: ['@nuxt/ui', '@nuxt/eslint'],
+
+	runtimeConfig: {
+		public: {
+			someValue: process.env.PUBLIC_SOME_VALUE,
+		},
+		openaiApiKey: process.env.OPENAI_API_KEY,
+	},
 });
